1ef8e646b66b352228bedd039d22747a84e1e993
sidechang <sidechang9388@gmail.com>
2025-06-30 23:46:01 +0900

    초기 스타일 설정

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,10 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Layout from "./components/layout";
 import Home from "./routes/home";
 import Profile from "./routes/profile";
+import CreateAccount from "./routes/create-account";
+import Login from "./routes/login";
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
 
 const router = createBrowserRouter([
   {
@@ -18,11 +22,33 @@ const router = createBrowserRouter([
       },
     ],
   },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/create-account",
+    element: <CreateAccount />,
+  },
 ]);
+
+//리셋 스타일 적용
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+    * {
+  box-sizing: border-box;
+  }
+  body {
+  background-color: black;
+  color: white;
+  font-family: 'system-ui', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+`;
 
 function App() {
   return (
     <>
+      <GlobalStyle />
       <RouterProvider router={router} />
     </>
   );
